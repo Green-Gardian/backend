eeab3c44cb367f68fbfbf82799a95570a4d5f208
Uzair Aslam <mruzairaslam1@gmail.com>
2025-05-24 11:07:12 +0500

    modified signUp function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -64,9 +64,8 @@ const signUp = async (req,res) => {
 
         const createdUser = await pool.query(insertQuery);
         console.log("Created User:",createdUser.rows[0])
-        const tokens = generateTokens(createdUser.rows[0]);
 
-        return res.status(200).json({message: `User created successfully`,...tokens})
+        return res.status(200).json({message: `User created successfully`,})
     }
     catch(error){
         return res.status(500).json({error: "Server Error"})
