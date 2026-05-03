ea3ba7d067edc27026302ce91e5704e7ea5bcac9
ukasha-dev <ukasha@nextpak.org>
2026-05-03 13:12:38 +0500

    Add Railway deployment config with production database setup

--- knexfile.js
+++ knexfile.js
@@ -14,5 +14,23 @@ module.exports = {
     migrations: {
       directory: "./migrations"
     }
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL || {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      port: process.env.DB_PORT,
+      ssl: { rejectUnauthorized: false }
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./migrations"
+    }
   }
 };
